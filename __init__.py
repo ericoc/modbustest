2cedabd82ba268237e2c484b51ae4e8c042e8442
Eric O'Callaghan <eric@ericoc.com>
2024-08-03 11:15:48 -0400

    argparse log tweaks

--- __init__.py
+++ __init__.py
@@ -5,8 +5,13 @@ from argparse import ArgumentParser
 from pymodbus.client import AsyncModbusTcpClient
 
 
+DEFAULT_HOST = "adam.home.ericoc.com"
+DEFAULT_PORT = 502
+DEFAULT_LOG_LEVEL = logging.WARNING
+
+
 async def main(
-    host: str = "adam.home.ericoc.com",
+    host: str = DEFAULT_HOST,
     port: int = 502,
     coils: (list, int, None) = None
 ):
@@ -24,7 +29,7 @@ async def main(
     for coil in coils:
 
         # Toggle the existing value of the coil, if requested.
-        if args.flip:
+        if args.toggle:
             original = await client.read_coils(address=coil, count=1)
             value = False
             if original.bits[0] is value:
@@ -43,16 +48,48 @@ async def main(
 if __name__ == "__main__":
 
     # Set up argument parser.
-    parser = ArgumentParser(description="ADAM-6060 unit testing")
-    parser.add_argument("-c", "--coil", dest="coil", type=int)
-    parser.add_argument("-f", "--flip", dest="flip", action="store_true")
+    parser = ArgumentParser(description="ADAM unit testing")
+    adam_unit = parser.add_argument_group(
+        title="ADAM unit",
+        description="Connection details for ADAM unit"
+    )
+    adam_unit.add_argument(
+        "-H", "--host", "--hostname",
+        dest="hostname",
+        default=DEFAULT_HOST,
+        help=f'ADAM unit hostname or IP address (default: "{DEFAULT_HOST}")'
+    )
+    adam_unit.add_argument(
+        "-p", "--port",
+        dest="port",
+        default=DEFAULT_PORT,
+        help=f"ADAM unit ModbusTCP port number (default: {DEFAULT_PORT})"
+    )
+    parser.add_argument(
+        "-c", "--coil",
+        dest="coil", type=int,
+        help="single coil number (default: range of 16-21)"
+    )
+    parser.add_argument(
+        "-f", "--flip", "-t", "--toggle",
+        dest="toggle", action="store_true",
+        help="toggle the boolean value of the coil"
+    )
     log_parser = parser.add_mutually_exclusive_group()
-    log_parser.add_argument("-v", "--verbose", dest="verbose", action="store_true")
-    log_parser.add_argument("-vv", "--debug", dest="debug", action="store_true")
+    log_parser.add_argument(
+        "-v", "--verbose",
+        dest="verbose", action="store_true",
+        help="verbose output"
+    )
+    log_parser.add_argument(
+        "-vv", "--debug",
+        dest="debug", action="store_true",
+        help="very verbose (debug) output"
+    )
     args = parser.parse_args()
 
     # Log level based on arguments (-v or -vv).
-    LOG_LEVEL = logging.WARNING
+    LOG_LEVEL = DEFAULT_LOG_LEVEL
     if args.verbose:
         LOG_LEVEL = logging.INFO
     if args.debug:
