fa56efc31c8a645b7e2ac7f49e6e4d41e5d8a2f8
Eric O'Callaghan <eric@ericoc.com>
2024-08-03 11:58:02 -0400

    only one verbose=>debug

--- __init__.py
+++ __init__.py
@@ -7,38 +7,44 @@ from pymodbus.client import AsyncModbusTcpClient
 
 DEFAULT_HOST = "adam.home.ericoc.com"
 DEFAULT_PORT = 502
-DEFAULT_LOG_LEVEL = logging.WARNING
+DEFAULT_COILS = range(16, 22)
+DEFAULT_LOG_LEVEL = logging.INFO
 
 
 async def main(
     host: str = DEFAULT_HOST,
-    port: int = 502,
-    coils: (list, int, None) = None
+    port: int = DEFAULT_PORT,
+    coils: (list, int) = DEFAULT_COILS,
+    toggle: bool = False,
 ):
     """ADAM unit interaction."""
+
+    # Connect to the ADAM unit.
     client = AsyncModbusTcpClient(host=host, port=port)
     await client.connect()
 
-    # Default to six (6) read-write coils: 16, 17, 18, 19, 20, 21.
-    if not coils:
-        coils = range(16, 22)
+    # Place single coil number in a list.
     if isinstance(coils, int):
         coils = [coils]
 
-    # Iterate each coil.
+    # Iterate each coil in the list.
     for coil in coils:
 
-        # Toggle the existing value of the coil, if requested.
-        if args.toggle:
+        # Handle requests to toggle coil(s).
+        if toggle:
+
+            # Get existing boolean coil value, to determine the new value.
             original = await client.read_coils(address=coil, count=1)
             value = False
             if original.bits[0] is value:
                 value = True
+
+            # Set the new boolean coil value.
             await client.write_coil(address=coil, value=value)
 
-        # Display the final value of the coil.
+        # Re-check the final value of the coil.
         recheck = await client.read_coils(address=coil, count=1)
-        logging.warning(f"Coil #{coil}: {recheck.bits[0]}")
+        logging.info(f"Coil #{coil}: {recheck.bits[0]}")
 
     # Disconnect from ADAM unit.
     client.close()
@@ -49,49 +55,42 @@ if __name__ == "__main__":
 
     # Set up argument parser.
     parser = ArgumentParser(description="ADAM unit testing")
-    adam_unit = parser.add_argument_group(
+    connect = parser.add_argument_group(
         title="ADAM unit",
-        description="Connection details for ADAM unit"
+        description="Connection details for ADAM unit, via Modbus TCP"
     )
-    adam_unit.add_argument(
+    connect.add_argument(
         "-H", "--host", "--hostname",
-        dest="hostname",
+        dest="host",
         default=DEFAULT_HOST,
-        help=f'ADAM unit hostname or IP address (default: "{DEFAULT_HOST}")'
+        help=f'ADAM Modbus TCP host or IP address (default: "{DEFAULT_HOST}")'
     )
-    adam_unit.add_argument(
-        "-p", "--port",
+    connect.add_argument(
+        "-P", "--port",
         dest="port",
         default=DEFAULT_PORT,
-        help=f"ADAM unit ModbusTCP port number (default: {DEFAULT_PORT})"
+        help=f"ADAM Modbus TCP port number (default: {DEFAULT_PORT})"
     )
-    parser.add_argument(
-        "-c", "--coil",
+    connect.add_argument(
+        "-C", "--coil",
         dest="coil", type=int,
-        help="single coil number (default: range of 16-21)"
+        default=DEFAULT_COILS,
+        help=f"ADAM Modbus TCP single coil number (default: {DEFAULT_COILS})"
     )
     parser.add_argument(
         "-f", "--flip", "-t", "--toggle",
         dest="toggle", action="store_true",
         help="toggle the boolean value of the coil"
     )
-    log_parser = parser.add_mutually_exclusive_group()
-    log_parser.add_argument(
-        "-v", "--verbose",
-        dest="verbose", action="store_true",
-        help="verbose output"
-    )
-    log_parser.add_argument(
-        "-vv", "--debug",
+    parser.add_argument(
+        "-v", "--debug",
         dest="debug", action="store_true",
         help="very verbose (debug) output"
     )
     args = parser.parse_args()
 
-    # Log level based on arguments (-v or -vv).
+    # Debug logging if requested (-v).
     LOG_LEVEL = DEFAULT_LOG_LEVEL
-    if args.verbose:
-        LOG_LEVEL = logging.INFO
     if args.debug:
         LOG_LEVEL = logging.DEBUG
 
@@ -99,9 +98,16 @@ if __name__ == "__main__":
     logging.basicConfig(
         datefmt="%Y-%m-%d %H:%M:%S %Z (%z)",
         format="%(asctime)s [%(levelname)s] (%(process)d): %(message)s",
-        handlers=[logging.StreamHandler()],
-        level=LOG_LEVEL
+        handlers=[logging.StreamHandler()], level=LOG_LEVEL
     )
 
     # Run main function async.
-    asyncio.run(main(coils=args.coil))
+    asyncio.run(
+        main(
+            host=args.host,
+            port=args.port,
+            coils=args.coil,
+            toggle=args.toggle
+        ),
+        debug=args.debug
+    )
