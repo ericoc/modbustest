23107aadc5cbb203f92ae0b4cf70619711ab7a61
Eric O'Callaghan <eric@ericoc.com>
2024-08-06 17:34:53 -0400

    unused params

--- config.example.py
+++ config.example.py
@@ -1,6 +1,6 @@
 DEBUG = True
-JSON_ADD_STATUS = DEBUG
-JSON_STATUS_FIELD_NAME = "status"
+#JSON_ADD_STATUS = DEBUG
+#JSON_STATUS_FIELD_NAME = "status"
 MODBUS_HOST = "adam.home.ericoc.com"
 MODBUS_COILS = range(16, 22)
 # SECRET_KEY = 'example123'
